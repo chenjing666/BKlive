5ab4f37374dfe3030a973400d21c3f7c9e9c7bcf
hwholiday@163.com <ai19950306>
2017-04-11 16:10:23 +0800

    直播顶部时间关系优化等晚上

---
+++ app/src/main/java/com/biaoke/bklive/bean/HeadBean.java
@@ -0,0 +1,21 @@
+package com.biaoke.bklive.bean;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+
+public class HeadBean {
+    private String imgUrl;
+
+    public HeadBean(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
