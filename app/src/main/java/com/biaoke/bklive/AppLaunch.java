97696140cf58ef25169cdcab3a93ce6defd30d3c
hasee <1774383254@qq.com>
2017-05-04 14:09:52 +0800

    添加caught,可以捕获用户程序应用时的错误，后期加入上传服务器

--- app/src/main/java/com/biaoke/bklive/AppLaunch.java
+++ app/src/main/java/com/biaoke/bklive/AppLaunch.java
@@ -16,7 +16,7 @@ public class AppLaunch extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //参数初始化化。
+        //参数初始化化
         StreamingEnv.init(getApplicationContext());
         //开启debug模式，方便定位错误，具体错误检查方式可以查看http://dev.umeng.com/social/android/quick-integration的报错必看，正式发布，请关闭该模式
         Config.DEBUG = true;
@@ -27,5 +27,10 @@ public class AppLaunch extends Application {
             PlatformConfig.setSinaWeibo("53400529", "87045c1fed3f93ef5fda9429352a2333","http://sns.whalecloud.com");
             PlatformConfig.setQQZone("1106024311", "FGnkSQttl6C4Wdd6");
         }
+        //异常捕获工具初始化
+        //上线了可以用来捕获用户产生的异常，把错误信息已文件的形式上传到服务器
+        //调试期用它就不打印log了，啊啊
+//        CrashHandler crashHandler = CrashHandler.getInstance();
+//        crashHandler.init(getApplicationContext());
     }
 }
