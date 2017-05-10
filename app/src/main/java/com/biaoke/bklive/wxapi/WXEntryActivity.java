a5fe941733e3bfbbb91e033db914d548f5af98ce
hasee <1774383254@qq.com>
2017-05-10 10:19:16 +0800

    完成签名打包

--- app/src/main/java/com/biaoke/bklive/wxapi/WXEntryActivity.java
+++ app/src/main/java/com/biaoke/bklive/wxapi/WXEntryActivity.java
@@ -5,7 +5,7 @@ package com.biaoke.bklive.wxapi;
 //import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 
-import com.umeng.weixin.callback.WXCallbackActivity;
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
 
 public class WXEntryActivity extends WXCallbackActivity {
 
